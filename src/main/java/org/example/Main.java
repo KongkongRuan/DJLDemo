68277af1565fed75d1b6bf84ea6e56e9c68c48a1
KongkongRuan <749868910@qq.com>
2023-12-06 15:53:23 +0800

    1.优化部分代码

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
         // 1. 加载 PyTorch 模型
         MNISTDetection mnistDetection = new MNISTDetection();
-//        String modelName = "j-cnn-gpu.pt";
-        String modelName = "j-cnn-cpu.pt";
+        String modelName = "j-cnn-gpu.pt";
+//        String modelName = "j-cnn-cpu.pt";
 
         Model model = mnistDetection.loadMode(modelName);
 
@@ -17,9 +17,9 @@ public class Main {
 //        int result1 = mnistDetection.predictImg(imgPath, model);
 //        System.out.println(result1);
 
-//        String imgPath = "D:\\input\\my\\";
-        String imgPath = "D:\\input\\";
-        for (int i = 0; i < 10; i++) {
+//        String imgPath = "D:\\input\\my\\"; int count = 5;
+        String imgPath = "D:\\input\\"; int count = 10;
+        for (int i = 0; i < count; i++) {
             int result = mnistDetection.predictImg(imgPath + i + ".png", model);
             System.out.println("Predict:"+result);
             System.out.println("Real:"+i);
